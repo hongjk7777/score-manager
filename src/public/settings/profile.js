7fd454b29e60fbf5e3499255263f5b9ab4de589f
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-07-15 18:19:11 +0900

    check user authentication to access user settings

--- src/public/settings/profile.js
+++ src/public/settings/profile.js
@@ -1,7 +1,8 @@
 import express from "express"
+import {isAuthenticated} from "../auth/auth.js"
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("settings/profile"));
+router.get("/", isAuthenticated, (req, res) => res.render("settings/profile"));
 
 export default router;
