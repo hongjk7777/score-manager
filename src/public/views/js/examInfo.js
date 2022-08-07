2c742e3290852fd7c3d6d1ee6a855603ab492baf
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-08-07 16:49:09 +0900

    add client logic and design to see their exams

--- src/public/views/js/examInfo.js
+++ src/public/views/js/examInfo.js
@@ -27,3 +27,59 @@ scoreRuleBtn.addEventListener("click", e => {
     const examRound = document.querySelector("#exam-round").innerHTML.charAt(0);
     location.href=`/class/score-rule?round=${examRound}`;
 });
+
+function makeChart() {
+    const examChart = document.getElementById('exam-chart');
+    const ctx = examChart.getContext('2d');
+    const chartDataStr = examChart.dataset.chart;
+    let chartData = chartDataStr.substring(1, chartDataStr.length - 1).split(',').map(x => parseInt(x));
+    let backgroundColors = getBackgroundColors();
+
+    const chart = new Chart(ctx, {
+        // type : 'bar' = 막대차트를 의미합니다.
+        type: 'bar', // 
+        data: {
+            labels: ['0~5', '6~10', '11~15', '16~20', '21~25', '26~30', '31~35', '36~40', '41~45', '46~50'],
+            datasets: [{
+                label: "내 점수",
+                // pointBackgroundColor: pointBackgroundColors,
+                backgroundColor: backgroundColors,
+                borderColor: 'rgb(255, 99, 132)',
+                data: chartData
+            }]
+        },
+        options: {
+            responsive: false,
+            legend: {
+                align: 'end',
+                position: 'top',
+            }
+        },
+        plugins: [{
+
+            beforeDraw: function(c) {
+            var legends = c.legend.legendItems;
+              legends.forEach(function(e) {
+                 e.fillStyle = 'rgb(255, 99, 132)';
+              });
+            }
+      
+          }]
+    });
+}
+
+
+function getBackgroundColors() {
+    let backgroundColors = Array(10).fill("gray");
+
+    const myScoreIndex = Math.ceil(parseInt(document.querySelector('#score-sum').innerHTML) / 5) - 1;
+    if(myScoreIndex < 0) {
+        myScoreIndex = 0;
+    }
+    
+    backgroundColors[myScoreIndex] = "rgb(255, 99, 132)";
+
+    return backgroundColors;
+}
+
+makeChart();
