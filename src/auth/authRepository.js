cf915d7090f0378ac7f4fdb54b949ecaf9d6db6d
hongjk7777 <hongjk7777@naver.com>
2022-12-31 18:46:50 +0900

    [add] save delete 기능 추가

--- src/auth/authRepository.js
+++ src/auth/authRepository.js
@@ -1,18 +1,38 @@
 import { Container } from "typedi";
-import db from "../db/dbConfig";
+import { asyncDB } from "../db/dbConfig";
+import User from "../model/user";
 
 export default class AuthRepository {
-    async findOneByUsername(username) {
-        const promiseDB = db.promise();
 
-        const [rows, fields] = await promiseDB.execute('SELECT * FROM accounts WHERE username = ?', [ username ]);
+    async save(user) {
+        const query = `INSERT INTO accounts(username, hashed_password, salt) 
+                        VALUE ('${user.getUsername()}', '${user.getHashedPassword()}', '${user.getSalt()}');`
+
+        const [results] = await asyncDB.execute(query);
+
+        return results.warningStatus === 0;
+    }
+
+    async findOneByUsername(username) {
+        const query = `SELECT * FROM accounts WHERE username = '${username}'`
+
+        const [rows] = await asyncDB.execute(query);
 
         if (rows.length === 0) {
             return null;
         }
 
-        return rows[0];
+        return new User(rows[0].username);
     }
+
+    async deleteByUsername(username) {
+        const query = `DELETE FROM accounts WHERE username = '${username}'`
+
+        const [results] = await asyncDB.execute(query);
+
+        return results.warningStatus === 0;
+    }
+
 }
 
 Container.set("authRepository", AuthRepository)
