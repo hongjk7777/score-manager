45bbb9e1e141e3858e5b226bad5b5bce5735f116
hongjk7777 <hongjk7777@naver.com>
2022-12-31 18:47:46 +0900

    [test] save delete select 테스트 추가

--- test/auth/authRepository.test.js
+++ test/auth/authRepository.test.js
@@ -1,12 +1,15 @@
 import AuthRepository from "../../src/auth/authRepository"
+import User from "../../src/model/user";
 
 const authRepository = new AuthRepository();
+
+//TODO: 추후에 sequalize를 사용해 테스트마다 디비를 초기화 시킬 수 있도록 해서 save delete 유닛 테스트 해야 함.
 
 describe( 'findOneByUsername 테스트', () => {
     test('정상 테스트', async () => {
         const user = await authRepository.findOneByUsername('test');
-
-        expect(user.username).toBe('test');
+        console.log(user);
+        expect(user.getUsername()).toBe('test');
     })
 
     test('유저가 존재하지 않을 경우', async () => {
@@ -15,3 +18,27 @@ describe( 'findOneByUsername 테스트', () => {
         expect(user).toBe(null);
     })
 })
+
+describe('통합 테스트', () => {
+    test('INSERT SELECT DELETE 테스트', async() => {
+        const user = getTempUser();
+
+        const saveResult = await authRepository.save(user);
+        expect(saveResult).toBe(true);
+        
+        let tempUser = await authRepository.findOneByUsername(user.getUsername());
+        expect(user.getUsername()).toBe(tempUser.getUsername());
+
+        const deleteResult = await authRepository.deleteByUsername(user.getUsername());
+        expect(deleteResult).toBe(true);
+
+        tempUser = await authRepository.findOneByUsername(user.getUsername());
+        expect(tempUser).toBe(null);
+
+    })
+})
+
+function getTempUser() {
+
+    return new User('realTestName', 'hashedPassword', 'salt');
+}
