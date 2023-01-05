1ca25e2a705a576f78a6f2240ab7449a8ca79005
hongjk7777 <hongjk7777@naver.com>
2023-01-05 21:00:53 +0900

    [feat] 다양한 오류 메시지 추가

--- src/validator/excelErrorMsg.js
+++ src/validator/excelErrorMsg.js
@@ -1,5 +1,8 @@
 export default class ExcelErrorMsg {
     static NO_PERSONAL_PAGE = '개인별 성적이 없습니다.';
-    static NO_EXAM_ROUND_INDEX = '회차 index가 올바르지 않습니다.'
-    static INCORRECT_EXAM_SCORE = '회차 성적이 올바르지 않습니다.'
+    static NO_PHONE_NUM_COL = '학부모 전화 번호 index가 없습니다.';
+    static NO_STUDENT_NAME_COL = '학부모 전화 번호 index가 없습니다.';
+    static INCORRECT_EXAM_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
+    static INCORRECT_COMMON_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
+    static INCORRECT_EXAM_SCORE = '회차 성적이 올바르지 않습니다.';
 }
