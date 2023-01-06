4641b5ccd0b636b79619e340a8dc6b4372686964
hongjk7777 <hongjk7777@naver.com>
2023-01-06 16:53:33 +0900

    [feat] 학생이 등록 안 되어 있을 경우 오류 처리 추가

--- src/validator/excelErrorMsg.js
+++ src/validator/excelErrorMsg.js
@@ -2,6 +2,7 @@ export default class ExcelErrorMsg {
     static NO_PERSONAL_PAGE = '개인별 성적이 없습니다.';
     static NO_PHONE_NUM_COL = '학부모 전화 번호 index가 없습니다.';
     static NO_STUDENT_NAME_COL = '학부모 전화 번호 index가 없습니다.';
+    static NO_EXISTENT_STUDENT = '해당하는 학생이 없습니다.';
     static INCORRECT_EXAM_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
     static INCORRECT_COMMON_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
     static INCORRECT_EXAM_SCORE = '회차 성적이 올바르지 않습니다.';
