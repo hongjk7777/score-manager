a765cbac7cc400be81ea77d0e7ffe517959aa002
hongjk7777 <hongjk7777@naver.com>
2023-01-08 12:53:53 +0900

    [test] worksheetService 테스트 작성

--- src/validator/excelErrorMsg.js
+++ src/validator/excelErrorMsg.js
@@ -3,7 +3,6 @@ export default class ExcelErrorMsg {
     static NO_PHONE_NUM_COL = '학부모 전화 번호 index가 없습니다.';
     static NO_STUDENT_NAME_COL = '학부모 전화 번호 index가 없습니다.';
     static NO_EXISTENT_STUDENT = '해당하는 학생이 없습니다.';
-    static INCORRECT_EXAM_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
     static INCORRECT_COMMON_ROUND_INDEX = '회차 index가 올바르지 않습니다.';
     static INCORRECT_EXAM_SCORE = '회차 성적이 올바르지 않습니다.';
 }
