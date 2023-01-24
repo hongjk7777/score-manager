70b129ccc6049f2bda3f74c00c5fc2a509861c69
hongjk7777 <hongjk7777@naver.com>
2023-01-24 17:57:39 +0900

    [test] 엑셀 테스트 수정

--- test/excel/excelService.test.js
+++ test/excel/excelService.test.js
@@ -5,6 +5,7 @@ import StudentRepository from "../../src/domain/db/student/studentRepository";
 import CourseService from "../../src/domain/service/courseService";
 import ExcelService from "../../src/excel/excelService";
 import WorksheetService from "../../src/excel/worksheetService";
+import ExcelErrorMsg from "../../src/validator/excelErrorMsg";
 
 const excelService = new ExcelService();
 const courseService = new CourseService();
@@ -32,9 +33,9 @@ afterAll(async () => {
 })
 
 async function createClass() {
-    expect(await courseRepository.save(testClassName)).toBe(true);
+    courseId = await courseRepository.save(testClassName);
 
-    const findCourse = await courseRepository.findByName(testClassName);
+    const findCourse = await courseRepository.findById(courseId);
     expect(findCourse.name).toBe(testClassName);
 
     return findCourse.id;
@@ -43,8 +44,9 @@ async function createClass() {
 async function deleteClass() {
     expect(await courseRepository.deleteById(courseId)).toBe(true);
 
-    const findCourse = await courseRepository.findByName(testClassName);
-    expect(findCourse).toBe(null);
+    await expect(courseRepository.findById(courseId))
+    .rejects
+    .toThrow(new ReferenceError(ExcelErrorMsg.NO_EXISTENT_CLASS));
 }
 
 describe('putExcelDatasToDB 테스트', () => {
@@ -55,12 +57,12 @@ describe('putExcelDatasToDB 테스트', () => {
         expect(students.length).toBe(66);
 
         const exams = await examRepository.findByClassId(courseId);
-        expect(exams.length).toBe(579);
+        expect(exams.length).toBe(580);
     }) 
 })
 
-describe('putExcelDatasToDB 테스트', () => {
-    test('정상 테스트', async () => {
+describe('putDeptDatasToDB 테스트', () => {
+    test.skip('정상 테스트', async () => {
         await excelService.putDeptDatasToDB(deptFile, courseId);
 
         const exams = await examRepository.findByClassId(courseId);
