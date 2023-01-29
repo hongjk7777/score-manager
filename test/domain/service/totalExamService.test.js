d2477e309a2f763850bbb03cf8bd19ad421cf43e
hongjk7777 <hongjk7777@naver.com>
2023-01-29 14:23:54 +0900

    [Test] totalExamService 테스트 코드 작성

--- test/domain/service/totalExamService.test.js
+++ test/domain/service/totalExamService.test.js
@@ -1,23 +1,104 @@
 import { async } from "regenerator-runtime";
+import TotalExam from "../../../src/domain/entity/totalExam";
+import CourseRepository from "../../../src/domain/repository/courseRepository";
+import TotalExamRepository from "../../../src/domain/repository/totalExamRepository";
+import CourseService from "../../../src/domain/service/courseService";
 import TotalExamService from "../../../src/domain/service/totalExamService";
 
-const totalService = new TotalExamService();
+const TEST_ROUND = 1;
+const TEST_COMMON_ROUND = 100;
 
-describe('countCommonExam 테스트', () => {
+const totalExamService = new TotalExamService();
+const totalExamRepository = new TotalExamRepository();
+
+const courseService = new CourseService();
+const courseRepository = new CourseRepository();
+
+let courseId;
+let testTotalExam;
+
+beforeAll(async () => {
+    await createTestCourse();
+})
+
+afterAll(async () => {
+    await courseService.deleteClassPrevDB(courseId);
+    await courseService.deleteClass(courseId);
+})
+
+async function createTestCourse() {
+    const testCourseName = 'totalExamServiceTest';
+
+    courseId = await courseRepository.save(testCourseName);
+
+    testTotalExam = createTestTotalExam();
+    await totalExamRepository.save(testTotalExam);
+}
+
+function createTestTotalExam() {
+    return new TotalExam(TEST_ROUND, TEST_COMMON_ROUND, 'scoreRule', courseId, 0, 0, 0, 0, [1, 2, 3]);
+}
+
+describe('getTotalExam 테스트', () => {
     test('정상 테스트', async () => {
-        const commonExamCount = await totalService.countCommonExam();
+        const findTotalExam = await totalExamService.getTotalExam(TEST_ROUND, courseId);
 
-        const realCommonExamCount = 4;
-        expect(commonExamCount).toBe(realCommonExamCount);
+        expect(findTotalExam).toEqual(testTotalExam);
     })
 })
 
-describe('getScoreRuleData 테스트', () => {
+describe('getProblemScores 테스트', () => {
     test('정상 테스트', async () => {
-        const commonRound = 2;
-        const scoreRule = await totalService.getCommonScoreRule(commonRound);
+        const findProblemScores = await totalExamService.getProblemScores(TEST_ROUND, courseId);
 
-        const scoreRuleLength = 93;
-        expect(scoreRule.length).toBe(scoreRuleLength);
+        expect(findProblemScores).toEqual(testTotalExam.problemScores);
+    })
+})
+
+describe('countCommonExam 테스트', () => {
+    test('정상 테스트', async () => {
+        const commonExamCount = await totalExamService.countCommonExam();
+
+        expect(commonExamCount).toBe(TEST_COMMON_ROUND);
+    })
+})
+
+describe('getCommonScoreRule 테스트', () => {
+    test('정상 테스트', async () => {
+        const scoreRule = await totalExamService.getCommonScoreRule(TEST_COMMON_ROUND);
+
+        expect(scoreRule[0][0]).toEqual(testTotalExam.scoreRule);
+    })
+})
+
+describe('getScoreRules 테스트', () => {
+    test('정상 테스트', async () => {
+        const findScoreRule = await totalExamService.getScoreRules(TEST_ROUND, courseId);
+
+        expect(findScoreRule[0]).toEqual(testTotalExam.scoreRule);
+    })
+})
+
+describe('getClassExamList 테스트', () => {
+    test('정상 테스트', async () => {
+        const findScoreRule = await totalExamService.getScoreRules(TEST_ROUND, courseId);
+
+        expect(findScoreRule[0]).toEqual(testTotalExam.scoreRule);
+    })
+})
+
+describe('getClassExamList 테스트', () => {
+    test('정상 테스트', async () => {
+        const classExamList = await totalExamService.getClassExamList(courseId);
+        
+        expect(classExamList[0]).toEqual(testTotalExam);
+    })
+})
+
+describe('getCommonRound 테스트', () => {
+    test('정상 테스트', async () => {
+        const commonRound = await totalExamService.getCommonRound(TEST_ROUND, courseId);
+        
+        expect(commonRound).toEqual(TEST_COMMON_ROUND);
     })
 })
